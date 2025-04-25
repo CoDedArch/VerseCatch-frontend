ca0396b74a737199ca62c95a69bcbbf64c89c2b0
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-04-25 08:56:19 +0000

    add a create account component

---
+++ src/shared/components/containers/CreatAccount.tsx
@@ -0,0 +1,28 @@
+// import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { logout } from "@/store/userSlice";
+import { useDispatch } from "react-redux";
+import { setIntroComplete } from "@/store/uiSlice";
+
+const CreateAccount = () => {
+  const dispatch = useDispatch();
+  
+
+  const handleCreateAccount = () => {
+    dispatch(setIntroComplete(false));
+    dispatch(logout());
+  };
+  return (
+    <motion.div
+      whileHover={{ scale: 1 }}
+      whileTap={{ scale: 0.98 }}
+      className="hover:bg-white/20 pl-2 rounded-lg font-bold text-lg flex items-center sm:gap-2 transition-all cursor-pointer no-highlight"
+      onClick={handleCreateAccount}
+    >
+      <img src="/assets/user.png" alt="Upgrade" className="w-5 sm:w-5" />
+      <span className="bg-slate-400/10 p-1 rounded">Create Account</span>
+    </motion.div>
+  );
+};
+
+export default CreateAccount;
